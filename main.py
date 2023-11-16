a6896f1e95eee8988ebf6b46ae285b93db33d10b
Aritz T <59333567+eltrevii@users.noreply.github.com>
2023-11-16 12:21:48 +0100

    add version selectr

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import ctypes
 ctypes.windll.kernel32.SetConsoleTitleA("TreviMC")
 
 versions = {
-    "1_8_9.ver":  "1.8.9",
-    "1_8_9.und":  "1_8_9",
-    "1_8_9.root": "1.8"
+    "1.8.9": ["1_8_9", "1.8"]
 }
 
-selected_ver = "1_8_9"
-
 print("welcome to TreviMC")
-print(f"version: {versions[selected_ver + '.root']}")
+
+print("select a version:")
+print(versions.keys())
+
+
+#print("version: " + versions[selected_ver + ".root"])
